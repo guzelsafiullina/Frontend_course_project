0bffb532fef65cb3646de483534307cdb4b7d1e0
guzelsafiullina <guzelsafiullina@gmail.com>
2022-10-27 16:34:58 +0300

    Add pages Text analysis and Question answerer.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,6 +25,12 @@
 			<li class:active={$page.url.pathname.startsWith('/sverdle')}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
+			<li class:active={$page.url.pathname.startsWith('/text_analysis')}>
+				<a href="/text_analysis">Text</a>
+			</li>
+			<li class:active={$page.url.pathname.startsWith('/question_answerer')}>
+				<a href="/question_answerer">Question Answerer</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
